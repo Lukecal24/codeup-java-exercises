b3faa1ac5092db11b65d55b4afb41e83aeb3c056
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-09 11:38:05 -0600

    exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,71 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //1
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+
+        //2
+        String myString = "Do you know da wey";
+        System.out.println(myString);
+
+        //3
+        char myChar = 'j';
+        myString = myChar + myString;
+        System.out.println(myString);
+
+        //4
+        double pi = 3.14159;
+        myString = myString + pi;
+        System.out.println(myString);
+
+        //5
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        //10
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+        //11
+//        int class = 5;
+//        System.out.println(class);
+
+        //12
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+
+        //13
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Aye");
+        System.out.println("Do you know da wey");
+        System.out.println(2 + 3);
+    }
+}
