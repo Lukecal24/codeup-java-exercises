1810758c186ac2cfd10f95b5aa8fc9a49eb56a03
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-10 09:06:58 -0600

    console exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -14,63 +17,6 @@ public class HelloWorld {
         char myChar = 'j';
         myString = myChar + myString;
         System.out.println(myString);
-
-        //4
-        double pi = 3.14159;
-        myString = myString + pi;
-        System.out.println(myString);
-
-        //5
-        float myNumber = 3.14F;
-        System.out.println(myNumber);
-
-        //10
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
-
-//        int x = 5;
-//        System.out.println(++x);
-//        System.out.println(x);
-
-
-        //11
-//        int class = 5;
-//        System.out.println(class);
-
-        //12
-//        String theNumberThree = "three";
-//        Object o = theNumberThree;
-//        int three = (int) o;
-//        System.out.println(three);
-
-
-        //13
-//        int x = 4;
-//        x += 5;
-//        System.out.println(x);
-
-//        int x = 3;
-//        int y = 4;
-//        y *= x;
-//        System.out.println(y);
-
-        int x = 10;
-        int y = 2;
-        x /= y;
-        y -= x;
-        System.out.println(y);
-
-
-        //14
-        byte b = 127;
-        b++;
-        System.out.println(b);
-
-
-
-
-
 
 
 
