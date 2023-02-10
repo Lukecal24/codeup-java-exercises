c2d9f6cb2a5d2a4aa09d0ef023b99316730e35b3
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-10 09:40:00 -0600

    finished exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,15 +5,15 @@ public class ConsoleExercises {
 
 
 
-        double pi = 3.14159;
-        System.out.println("The value of pi is approximately " + String.format("%.2f", pi) + ".");
-//1
+//        double pi = 3.14159;
+//        System.out.println("The value of pi is approximately " + String.format("%.2f", pi) + ".");
+////1
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter a integer");
 //        int userInput = scanner.nextInt();
 //        System.out.println("You entered " + userInput);
-//
-////        2
+////
+//////        2
 //        System.out.print("Enter the first word: ");
 //        String word1 = scanner.next();
 //        System.out.print("Enter the second word: ");
@@ -21,15 +21,32 @@ public class ConsoleExercises {
 //        System.out.print("Enter the third word: ");
 //        String word3 = scanner.next();
 //        System.out.println("The three words are:");
-//        System.out.println(word1);
-//        System.out.println(word2);
-//        System.out.println(word3);
+////        System.out.print(word1 + "\n" + word2 + "\n" + word3);
+//        System.out.printf("%s\n%s\n%s", word1, word2, word3);
 
 //        3
 //        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
-        System.out.println("Enter a sentence");
-        String userSentence = scanner.nextLine();
-        System.out.println("You're sentence was:");
-        System.out.println(userSentence);
+//        System.out.print("Enter a sentence");
+//        String userSentence = scanner.nextLine();
+//        System.out.println("You're sentence was:");
+//        System.out.println(userSentence);
+
+
+
+//        last one
+        System.out.println("Enter the length of codeup");
+        double length = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Enter the width");
+        double width = Double.parseDouble(scanner.nextLine());
+
+        double perimeter = (length + width) * 2;
+        double area = length * width;
+        System.out.println("The perimeter is " + perimeter);
+        System.out.println("The area is " + area);
+
+
+
+
     }
 }
