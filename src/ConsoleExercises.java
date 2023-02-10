322d27324972f39d0c636ed5495680b3bd3e4775
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-10 10:09:17 -0600

    finished exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,27 +9,28 @@ public class ConsoleExercises {
 //        System.out.println("The value of pi is approximately " + String.format("%.2f", pi) + ".");
 ////1
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter a integer");
-//        int userInput = scanner.nextInt();
-//        System.out.println("You entered " + userInput);
-////
-//////        2
-//        System.out.print("Enter the first word: ");
-//        String word1 = scanner.next();
-//        System.out.print("Enter the second word: ");
-//        String word2 = scanner.next();
-//        System.out.print("Enter the third word: ");
-//        String word3 = scanner.next();
-//        System.out.println("The three words are:");
-////        System.out.print(word1 + "\n" + word2 + "\n" + word3);
-//        System.out.printf("%s\n%s\n%s", word1, word2, word3);
+        System.out.println("Enter a integer");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered " + userInput);
+//
+////        2
+        System.out.print("Enter the first word: ");
+        String word1 = scanner.next();
+        System.out.print("Enter the second word: ");
+        String word2 = scanner.next();
+        System.out.print("Enter the third word: ");
+        String word3 = scanner.next();
+        System.out.println("The three words are:");
+//        System.out.print(word1 + "\n" + word2 + "\n" + word3);
+        System.out.printf("%s\n%s\n%s", word1, word2, word3);
 
 //        3
 //        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
-//        System.out.print("Enter a sentence");
-//        String userSentence = scanner.nextLine();
-//        System.out.println("You're sentence was:");
-//        System.out.println(userSentence);
+        System.out.print("Enter a sentence");
+        scanner.nextLine();
+        String userSentence = scanner.nextLine();
+        System.out.println("You're sentence was:");
+        System.out.println(userSentence);
 
 
 
