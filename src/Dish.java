b2343f152eda7e177fb22d742da70a54bead7cc9
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-14 12:30:21 -0600

    added separate dishTest file

--- src/Dish.java
+++ src/Dish.java
@@ -25,20 +25,4 @@ Add a main method and inside the method...
         System.out.printf("Cost: %d cents%n", costInCents);
         System.out.printf("Would recommend: %b%n", wouldRecommend);
     }
-
-
-    public static void main(String[] args){
-        Dish dish1 = new Dish();
-        dish1.costInCents = 55;
-        dish1.nameOfDish = "Spaghetti";
-        dish1.wouldRecommend = true;
-
-
-        dish1.printSummary();
-
-
-
-
-
-    }
 }
