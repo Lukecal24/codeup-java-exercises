6a8ac3184aa82111507a244714b1f219552fa6d3
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-17 14:16:51 -0600

    finished Inheritance and polymorphism exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,8 +2,23 @@ package shapes;
 
 public class Square extends Rectangle{
 
+private static int side;
+
+
     public Square(int side) {
         super(side, side);
+    }
+
+
+    public int getArea(){
+        int side = super.getLength();
+        return side * side;
+    }
+
+
+    public int getPerimeter(){
+        int side = super.getLength();
+        return side * 4;
     }
 
 
