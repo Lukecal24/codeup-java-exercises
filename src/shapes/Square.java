e48034cf51f41e99c18d22c673b6e2d75e892992
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-21 14:44:47 -0600

    quadrilateral class started

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,19 +2,19 @@ package shapes;
 
 public class Square extends Rectangle{
 
-    public Square(int side) {
-        super(side, side);
-    }
-
-    @Override
-    public int getArea(){
-        return length * width;
-    }
-
-    @Override
-    public int getPerimeter(){
-        return length * 4;
-    }
+//    public Square(int side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public int getArea(){
+//        return length * width;
+//    }
+//
+//    @Override
+//    public int getPerimeter(){
+//        return length * 4;
+//    }
 
 
 }
