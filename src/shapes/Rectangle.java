986eff12e942a24174ac35a208374bf67c3c2c7d
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-22 09:04:59 -0600

    finished exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -26,36 +26,4 @@ public class Rectangle extends Quadrilateral {
     }
 
 
-    //    protected int length;
-//    protected int width;
-//
-//    public Rectangle(int length, int width) {
-//        this.length = length;
-//        this.width = width;
-//    }
-//
-//    public int getLength() {
-//        return length;
-//    }
-//
-//    public void setLength(int length) {
-//        this.length = length;
-//    }
-//
-//    public int getWidth() {
-//        return width;
-//    }
-//
-//    public void setWidth(int width) {
-//        this.width = width;
-//    }
-//
-//    public int getArea(){
-//        return length * width;
-//    }
-//
-//    public int getPerimeter(){
-//        return (length + width) * 2;
-//    }
-
 }
