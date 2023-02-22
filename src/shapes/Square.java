1569d0315d97d4084b98f14af7c81a2c4a203fc3
Luke Calderon <luke.a.calderon@gmail.com>
2023-02-22 09:33:29 -0600

    re-factored code

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,46 +2,35 @@ package shapes;
 
 public class Square extends Quadrilateral{
 
-    protected int side;
+
 
     public Square( int side) {
         super(side, side);
-        this.side = side;
+
     }
 
     @Override
     public double getPerimeter() {
-        return side * 4;
+        return length * 4;
     }
 
     @Override
     public double getArea() {
-        return side * side;
+        return length * length;
     }
 
     @Override
     public void setLength(double length) {
-
+        this.length = length;
+        setWidth(length);
     }
 
     @Override
     public void setWidth(double width) {
-
+        this.width = width;
+        setLength(width);
     }
 
-//    public Square(int side) {
-//        super(side, side);
-//    }
-//
-//    @Override
-//    public int getArea(){
-//        return length * width;
-//    }
-//
-//    @Override
-//    public int getPerimeter(){
-//        return length * 4;
-//    }
 
 
 }
